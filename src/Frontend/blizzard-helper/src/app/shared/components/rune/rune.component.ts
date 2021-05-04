b7ada9b9593a8757d5092a91799b67275e03e561
Bence, Mihucza <mihben@gmail.com>
2021-05-04 19:10:34 +0200

    Add tooltip to rune

--- src/Frontend/blizzard-helper/src/app/shared/components/rune/rune.component.ts
+++ src/Frontend/blizzard-helper/src/app/shared/components/rune/rune.component.ts
@@ -9,11 +9,21 @@ import { Rune } from '../../models/rune.model';
 
 export class RuneComponent implements OnInit {
   @Input() assets: string = '';
-  @Input() rune: Rune = { id: '', name: '' };
-  
+  @Input() rune: Rune = { id: '', name: '', level: 0, inArmor: '', inHelm: '', inShield: '', inWeapon: '' };
+  @Input() level: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public isAdoptable() : string {
+    if (this.rune != undefined && this.level >= this.rune.level) return "adoptable";
+    else return "unadoptable"
+  }
+
+  public isAdoptableName() : string {
+    if (this.rune != undefined && this.level >= this.rune.level) return "name";
+    else return "name-unadoptable"
+  }
 }
--- src/Frontend/blizzard-helper/src/app/shared/models/rune.model.ts
+++ src/Frontend/blizzard-helper/src/app/shared/models/rune.model.ts
@@ -1,4 +1,9 @@
 export interface Rune {
     id: string;
     name: string;
+    level: number;
+    inWeapon: string;
+    inArmor: string;
+    inHelm: string;
+    inShield: string;
 }
