0a7f79f66c5f026d3edeb600f678d97c2e1ca495
Bence Mihcua <mihben@gmail.com>
2021-06-16 02:59:45 +0200

    First steps

---
+++ src/Backend/DiabloII-Cookbook.Client/Extensions/CharacterExtensions.cs
@@ -0,0 +1,13 @@
+﻿using DiabloII_Cookbook.Api.DataTransferObjects;
+using System;
+
+namespace DiabloII_Cookbook.Client.Extensions
+{
+    public static class CharacterExtensions
+    {
+        public static string GetImage(this Character character)
+        {
+            return $"/assets/classic/classes/{character.Class.ToLower()}.gif";
+        }
+    }
+}
