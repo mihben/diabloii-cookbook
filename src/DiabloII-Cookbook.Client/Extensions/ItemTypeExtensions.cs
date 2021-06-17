42fa70d92789087955096f7e5e988dc804efee84
Bence Mihcua <mihben@gmail.com>
2021-06-18 00:12:25 +0200

    Add item type filter

---
+++ src/DiabloII-Cookbook.Client/Extensions/ItemTypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using DiabloII_Cookbook.Api.DataTransferObjects;
+using DiabloII_Cookbook.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiabloII_Cookbook.Client.Extensions
+{
+    public static class ItemTypeExtensions
+    {
+        public static IEnumerable<Filter> AsFilter(this IEnumerable<ItemType> itemTypes, string group)
+        {
+            return itemTypes.Where(it => it.Group.Equals(group, StringComparison.InvariantCultureIgnoreCase)).Select(it => new Filter(it));
+        }
+    }
+}
---
+++ src/DiabloII-Cookbook.Client/Models/Filter.cs
@@ -0,0 +1,18 @@
+﻿using DiabloII_Cookbook.Api.DataTransferObjects;
+using System.ComponentModel;
+
+namespace DiabloII_Cookbook.Client.Models
+{
+    public class Filter
+    {
+        private bool _selected;
+
+        public bool Selected { get { return _selected; } set { _selected = value; System.Console.WriteLine("Setted selected"); } }
+        public ItemType ItemType { get; }
+
+        public Filter(ItemType itemType)
+        {
+            ItemType = itemType;
+        }
+    }
+}
